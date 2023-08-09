d4fd4e7e23a7d13029f77bacc43349798dd2e0d1
eltonmessiass <eltonmessias10@gmail.com>
2023-08-10 00:11:49 +0200

    funcao de update concluida

--- CRM/urls.py
+++ CRM/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path("", views.home, name="home"),
     # path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
-    path('register/', views.register_user, name="register"),\
-    path('addRecord/', views.CreateRecord, name='add-record')
+    path('register/', views.register_user, name="register"),
+    path('addRecord/', views.CreateRecord, name='add-record'),
+    path('record/<str:pk>/', views.costumer_record, name="c_record"),
+    path('edit-record/<str:pk>/', views.edit_record, name="edit-record")
 
 ]
 
